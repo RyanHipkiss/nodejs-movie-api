d9401121417c9a6c8088a7fdf1911004bde7866d
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2023-03-25 17:03:10 +0000

    basic api endpoint for movies

---
+++ index.ts
@@ -0,0 +1,57 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+const app = express()
+app.use(helmet())
+app.use(morgan())
+app.use(cors())
+app.use(bodyParser.json())
+
+type Movie = {
+    title: string
+    image: string
+    thumbnailImage: string
+    genre: string
+}
+
+app.use('/movies', (req: Request, res: Response): express.Response<Movie[]> => {
+    return res.send([
+        {
+            title: 'The Awesome Title of a Movie',
+            image: 'https://picsum.photos/1920/1080',
+            thumbnailImage: 'https://picsum.photos/300/400',
+            genre: 'Horror'
+        },
+        {
+            title: 'Short Title',
+            image: 'https://picsum.photos/1920/1080',
+            thumbnailImage: 'https://picsum.photos/300/400',
+            genre: 'Horror'
+        },
+        {
+            title: 'Why do we do this?',
+            image: 'https://picsum.photos/1920/1080',
+            thumbnailImage: 'https://picsum.photos/300/400',
+            genre: 'Horror'
+        },
+        {
+            title: 'Deliberately worded!',
+            image: 'https://picsum.photos/1920/1080',
+            thumbnailImage: 'https://picsum.photos/300/400',
+            genre: 'Horror'
+        },
+        {
+            title: 'What?!',
+            image: 'https://picsum.photos/1920/1080',
+            thumbnailImage: 'https://picsum.photos/300/400',
+            genre: 'Horror'
+        }
+    ])
+})
+
+app.listen(3001, () => {
+    console.log('listening on port 3001')
+})
